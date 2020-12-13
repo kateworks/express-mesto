7bd6975f3e0aa57fba566cc6fd8942247d5d3e98
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2020-12-13 14:04:34 +0300

    feat: создать схемы и модели (users, cards)

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes/index.js');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
