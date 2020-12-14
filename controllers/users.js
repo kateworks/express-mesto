67e7ff101972a0221feeeded1a58c227be63c4f3
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2020-12-14 17:53:11 +0300

    feat: реализовано обновление аватара пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -56,3 +56,17 @@ module.exports.updateProfile = (req, res) => {
       res.status(ERROR_SERVER).send({ message: 'Ошибка на сервере' });
     });
 };
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true, upsert: true },
+  )
+    .then((user) => res.status(STATUS_OK).send({ data: user }))
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+      res.status(ERROR_SERVER).send({ message: 'Ошибка на сервере' });
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,15 @@ const {
   getUser,
   createUser,
   updateProfile,
+  updateAvatar,
 } = require('../controllers/users.js');
 
 router.get('/users', getUsers);
-
 router.get('/users/:id', getUser);
 
 router.post('/users', createUser);
 
 router.patch('/users/me', updateProfile);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
