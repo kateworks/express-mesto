54b40cbdc86783b20042ade74d3aa78c14f06efe
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2020-12-14 13:13:34 +0300

    feat: реализовать временное решение авторизации

--- app.js
+++ app.js
@@ -8,17 +8,24 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(bodyParser.json());
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5fd7172deeb1f02214d978a9',
+  };
+  next();
+});
 
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
