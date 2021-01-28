37e678ba0377591f34451cf6662c9ef4f73c06d7
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2021-01-28 12:19:03 +0300

    fix: исправить ошибку

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Подключаем логгер запросов
